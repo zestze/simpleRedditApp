0d8b0d912d00dec2772062eae4752d9a5ea3b4ee
zestze <eer2138@columbia.edu>
2018-05-29 13:24:09 -0500

    in the middle of switching to class oriented attempt

--- simpleClient.py
+++ simpleClient.py
@@ -1,85 +1,69 @@
 #!/usr/bin/env python3
 """
 simple python client for grabbing a users saved links by subreddit
+
+@TODO: replace ZestyZeke with username vals
 """
 
 import requests
 import requests.auth
 import string
 import re
+import sys
 
 _USER_AGENT_ = "simpleRedditClient/0.1 by ZestyZeke"
 
-def grabCredentials():
-    p = ""
-    u = ""
-    cI = ""
-    cS = ""
-    with open('client.id', 'r') as myFile:
-        cI = myFile.read().replace('\n', '')
-    with open('client.secret', 'r') as myFile:
-        cS = myFile.read().replace('\n', '')
-    with open('user.name', 'r') as myFile:
-        u = myFile.read().replace('\n', '')
-    with open('user.pass', 'r') as myFile:
-        p = myFile.read().replace('\n', '')
-    return u, p, cI, cS
+class UserAPIobject:
+    username = ""
+    password = ""
+    clientID = ""
+    clientSecret = ""
+    def __init__(self):
+        with open('client.id', 'r') as file1, \
+                open('client.secret', 'r') as file2, \
+                open('user.name', 'r') as file3, \
+                open('user.pass', 'r') as file4:
+            self.clientID = file1.read().replace('\n', '')
+            self.clientSecret = file2.read().replace('\n', '')
+            self.username = file3.read().replace('\n', '')
+            self.password = file4.read().replace('\n', '')
+
+    def authorize(self):
+        client_auth = requests.auth.HTTPBasicAuth(clientID, clientSecret)
+        post_data = {"grant_type": "password", "username": self.username,
+                     "password": self.password}
+        headers = {"User-Agent": _USER_AGENT_}
+        response = requests.post("https://www.reddit.com/api/v1/access_token",
+                                 auth=client_auth, data=post_data,
+                                 headers=headers)
 
 def parse_response(responseDict, filterSubreddit):
+    """
+    responseDict['children'] is a list of children.
+    Each has multiple attributes like
+    url, title, subreddit, name
+    """
     for childDict in responseDict['children']:
         dataDict = childDict['data']
-        if filterSubreddit == "" or dataDict['subreddit'] == filterSubreddit:
+        if filterSubreddit == "" or dataDict['subreddit'].lower() == filterSubreddit.lower():
             print ("https://www.reddit.com" + dataDict['permalink'])
 
-        # print url, title and subreddit
-        #print ("url: " + dataDict['url'])
-        #print ("title: " + dataDict['title'])
-        #print ("subreddit: " + dataDict['subreddit'])
-        #print ("name: " + dataDict['name'])
-    #print (responseDict.keys())
     done = False
     if len(responseDict['children']) == 0:
         done = True
     return responseDict['after'], done
 
-def give_url(subreddit, title, name):
-    """
-    format is
-    https://www.reddit.com/r/<subredditWithCaps>/comments/<partOfName>/<title_no_caps>
-    the name should be something like <t3_id>, <partOfName> refers to the 'id' part
-    the <title_no_caps> should also replace spaces with underscores
-    """
-    url = "https://www.reddit.com/r/{}/comments/".format(subreddit)
-    nameID = name[name.find("_")+1:]
-    title = title.lower().replace(' ', '_')
-    punct = string.punctuation.replace('_', '')
-    punct += "|"
-
-    outtab = ''.join(["+" for p in punct])
-
-    title = title.translate(str.maketrans(punct, outtab))
-    title = title.replace('+', '')
-
-    title = re.sub('_+', '_', title)
-    title = title[:48]
-    if title[-1] == "_":
-        title = title[:len(title) - 1]
-    if title[0] == "_":
-        title = title[1:]
-
-    url += "{}/{}".format(nameID, title)
-
-    return url
-
 def check_rate_limit(response):
+    """
+    Xused - used requests
+    Xrem - remaining requests
+    Xres - seconds to end of period
+    """
     Xused = response.headers['X-Ratelimit-Used']
     Xrem = response.headers['X-Ratelimit-Remaining']
     Xres = response.headers['X-Ratelimit-Reset']
     if Xrem == "0": # note: is of type str
         raise RuntimeError("no remaining requests for this period") 
-    #print ("Used Requests: " + Xused)
-    #print ("Remaining Requests: " + Xrem)
-    #print ("Seconds to end of period: " + Xres)
     return Xused, Xrem, Xres
 
 def authorize():
@@ -124,5 +108,9 @@ def main(filterSubreddit=""):
             afterSet.add(after)
 
 if __name__ == "__main__":
-    f = "HunterXHunter"
-    main(f)
+    if len(sys.argv) != 2:
+        f = "hunterxhunter"
+        #f = "youtubehaiku"
+        main(f)
+    else:
+        main(sys.argv[1])
